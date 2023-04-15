2a779d65bd93401b13b3a451a57da6b8f90b7510
Arbaz Hussain <arbazh777@gmail.com>
2023-04-15 15:28:20 +0530

    Added routes for overview and tour pages
    Added route handlers for "/overview" and "/tour" in the app file to render the respective pug templates. Also, created the base template with placeholder content for the child templates.
    
    The "overview.pug" and "tour.pug" templates are extended from the "base.pug" template and contain their own "block content" sections.

--- app.js
+++ app.js
@@ -68,8 +68,20 @@ app.use(cors());
 
 app.get("/", (req, res) => {
   res.status(200).render("base", {
-    tour: "1",
+    tour: "The Forest Hiker",
     user: "Arbaz",
+  });
+});
+
+app.get("/overview", (req, res) => {
+  res.status(200).render("overview", {
+    title: "All Tours",
+  });
+});
+
+app.get("/tour", (req, res) => {
+  res.status(200).render("tour", {
+    title: "The Forest Hiker Tour",
   });
 });
 
