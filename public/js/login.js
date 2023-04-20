d21f3f56be6657e2e03427c89437edd90bcaed98
Arbaz Hussain <arbazh777@gmail.com>
2023-04-20 13:12:36 +0530

    Add comments to improve the readability and maintainability of login.js file

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,13 @@
+// Selecting the form element using its class name
 const form = document.querySelector(".form");
 
+// Defining a function to handle login
 const login = async (email, password) => {
+  // Logging the email and password to the console
   console.log({ email, password });
+
   try {
+    // Sending a POST request to the login endpoint with email and password in the request body
     const res = await fetch("http://localhost:8080/api/v1/users/login", {
       method: "POST",
       headers: {
@@ -14,9 +19,13 @@ const login = async (email, password) => {
       }),
     });
 
+    // Parsing the response data
     let data = await res.json();
 
+    // Logging the response data to the console
     console.log(data);
+
+    // Showing an alert message if the login was successful and redirecting to the home page
     if (data.status === "success") {
       alert("Logged in Successfully!");
 
@@ -24,18 +33,24 @@ const login = async (email, password) => {
         location.assign("/");
       }, 1500);
     } else {
+      // Showing an alert message with the error message if the login failed
       alert(data.message);
     }
   } catch (error) {
+    // Showing an alert message with the error message if there was an error while sending the request
     alert(error.message);
   }
 };
 
+// Adding an event listener to the form for the submit event
 form.addEventListener("submit", (e) => {
+  // Preventing the default form submission behavior
   e.preventDefault();
 
+  // Getting the email and password values from the form inputs
   let email = form.email.value;
   let password = form.password.value;
 
+  // Calling the login function with email and password values
   login(email, password);
 });
