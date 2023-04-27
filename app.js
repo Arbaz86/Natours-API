d4369d817541a65a2ba14f25e927efc7b498a588
Arbaz Hussain <arbazh777@gmail.com>
2023-04-27 23:41:30 +0530

    Create bookingController and bookingRoutes with Stripe integration
    The following changes were made:
    
    Created bookingController in the controller folder and added a function to get the checkout session for a booked tour using Stripe API.
    Created bookingRoutes in the routes folder and added a GET route for getting the checkout session using the bookingController function.
    Added authentication middleware to the bookingRoutes to ensure that only authenticated users can access the route.
    Registered the bookingRoutes in the app.js file using the /api/v1/bookings base URL.
    Installed the Stripe package in the project.

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
 const viewRouter = require("./routes/viewRoutes");
+const bookingRouter = require("./routes/bookingRoutes");
 
 const app = express();
 
@@ -103,6 +104,7 @@ app.use("/", viewRouter);
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/reviews", reviewRouter);
+app.use("/api/v1/bookings", bookingRouter);
 
 // Handle requests that do not match any of the defined routes
 app.all("*", (req, res, next) => {
---
+++ controllers/bookingController.js
@@ -0,0 +1,42 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const express = require("express");
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
+const Tour = require("../models/tourModel");
+
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+  // 1) Get the currently booked tour
+  const { tourId } = req.params;
+
+  const tour = await Tour.findById(tourId);
+
+  // 2) Create checkout session
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    mode: "payment",
+    success_url: `${req.protocol}://${req.get("host")}/`,
+    cancel_url: `${req.protocol}://${req.get("host")}/tour/${tour.slug}`,
+    customer_email: req.user.email,
+    client_reference_id: tourId,
+    line_items: [
+      {
+        price_data: {
+          currency: "inr",
+          product_data: {
+            name: `${tour.name} Tour`,
+            description: tour.summary,
+            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+          },
+          unit_amount: tour.price * 80,
+        },
+        quantity: 1,
+      },
+    ],
+  });
+
+  // 3) Create session as response
+  res.status(200).json({
+    status: "success",
+    session,
+  });
+});
