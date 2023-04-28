a1342b2974393508ca2408ea3cbab9a02499be91
Arbaz Hussain <arbazh777@gmail.com>
2023-04-28 17:27:20 +0530

    Added Booking model schema in bookingModel.js file in the models folder
    This commit adds the Mongoose schema for Booking model to the bookingModel.js file. It defines the schema with fields such as tour, user, price, createdAt, and paid. It also defines a pre-query middleware to populate the user and tour fields. Finally, it creates the Booking model using the schema and exports it for use in other parts of the application.

---
+++ models/bookingModel.js
@@ -0,0 +1,47 @@
+// Importing Mongoose package
+const mongoose = require("mongoose");
+
+// Creating a booking schema using Mongoose schema constructor
+const bookingSchema = new mongoose.Schema({
+  // Reference to the Tour model
+  tour: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Tour",
+    required: [true, "Booking must belong to a Tour!"],
+  },
+  // Reference to the User model
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: [true, "Booking must belong to a User!"],
+  },
+  // Price of the booking
+  price: {
+    type: Number,
+    required: [true, "Booking must have a price"],
+  },
+  // Date and time of the booking creation
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  // Whether the booking has been paid or not
+  paid: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+// Pre-query middleware to populate the user and tour fields
+bookingSchema.pre(/^find/, function () {
+  this.populate("user").populate({
+    path: "tour",
+    select: "name",
+  });
+});
+
+// Creating the Booking model using the booking schema
+const Booking = mongoose.model("Booking", bookingSchema);
+
+// Exporting the Booking model
+module.exports = Booking;
